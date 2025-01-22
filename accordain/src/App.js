2b8691e51a8eb6475be6af552ddb13150180d2ec
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-01-22 22:28:49 +0100

    rating

--- accordain/src/App.js
+++ accordain/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Accordian from './components/Accordains';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
      <Accordian />
      <RandomColor />
+     <StarRating />
     </div>
   );
 }
