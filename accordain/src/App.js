c2453fe9e68c15fa03761af720f7f32bb3c4d300
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-03-10 17:28:10 +0100

    menus

--- accordain/src/App.js
+++ accordain/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import Accordian from './components/Accordains';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
-import ImageSlider from './components/image-slider';
-import LoadMoreData from './components/load-more-data';
+import logo from "./logo.svg";
+import "./App.css";
+import Accordian from "./components/Accordains";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
+import TreeView from "./components/tree-veiw ";
 
 function App() {
   return (
     <div className="App">
-     <Accordian />
-     <RandomColor />
-     <StarRating />
-     <ImageSlider
-     url={"https://picsum.photos/v2/list"}
-     page={"1"}
-     limit={"10"} />
-     <LoadMoreData />
+      <Accordian />
+      <RandomColor />
+      <StarRating />
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
+      <LoadMoreData />
+      {/* Tree view component/menu UI component / recursive navigation menu */}
+      {/* <TreeView menus={menus} /> */}
+      <TreeView  />
     </div>
   );
 }
